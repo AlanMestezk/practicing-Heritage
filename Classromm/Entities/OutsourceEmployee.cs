9ad40fd5ac1cd788fc34389a25de608a0945c67b
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-09-23 16:58:03 -0300

    praticing Heritage

---
+++ Classromm/Entities/OutsourceEmployee.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Classromm.Entities
+{
+    internal class OutsourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourceEmployee() { }
+
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge):
+            base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
---
+++ Classromm/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+using Classromm.Entities;
+
+using System.Globalization;
+
+
+
+namespace Classromm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> list = new();
+
+            Console.WriteLine();
+
+            Console.WriteLine("  =====================================");
+            Console.WriteLine("               app employee");
+            Console.WriteLine("  =====================================");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("             Enter the number ");
+            Console.Write("                 of the employess: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"        Employee #{i} data: ");
+                Console.Write("             Outsourced (y/n)?: ");
+                string ch = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine(".........................................");
+                Console.Write("         Name: ");
+                string name = Console.ReadLine();
+                Console.Write("         Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("             Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch =="y")
+                {
+                    Console.Write("         Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("          ***********************************");
+            Console.WriteLine("                       PAYMENTS");
+            Console.WriteLine();
+            
+            foreach(Employee emp in list)
+            {
+                Console.WriteLine($"        {emp.Name} - $ {emp.Payment().ToString("f2", CultureInfo.InstalledUICulture)}");
+            }
+
+        }
+    }
+}
